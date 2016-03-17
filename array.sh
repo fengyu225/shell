c7298616362c4475c6a0c532839830ce7673d807
Yu Feng <yfeng@boingo.com>
2016-03-16 18:00:25 -0700

    A  array.sh M  case.sh

---
+++ array.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for i in {0..9} {a..z} {A..Z};do
+    echo $i;
+done;
--- case.sh
+++ case.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-case "$1" in
+case $1 in
     "abc") echo "abc" ;;
     "def") echo "def" ;;
     "ghi") echo "ghi" ;;
